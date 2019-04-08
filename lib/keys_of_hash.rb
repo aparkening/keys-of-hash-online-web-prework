fe186fe2ef5ed7ce6ce6148d408fc41661fd4915
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-08 21:36:44 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -13,9 +13,9 @@ animals = {
 class Hash
   def keys_of(*arguments)
     
-    self.map { |key, value| arguments.include?(value) key : nil }
+    self.map { |key, value| arguments.include?(value) ? key : nil }.compact
     
-    delete_if?()
+    
     
     
 =begin    
