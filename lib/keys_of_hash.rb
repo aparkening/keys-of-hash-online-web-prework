456dd092a69854e0636cc381a1e1c2410be19fb7
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-08 21:45:24 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
-animals = {
-  "sugar glider"=>"Australia",
-  "aye-aye"=> "Madagascar",
-  "red-footed tortoise"=>"Panama",
-  "kangaroo"=> "Australia",
-  "tomato frog"=>"Madagascar",
-  "koala"=>"Australia"
-}
-
 # Return array with every key marching input arguments
 class Hash
   def keys_of(*arguments)
