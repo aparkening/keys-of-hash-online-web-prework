21dfd45dc7e688ae0c4149cd719749fef65e12f2
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-08 21:15:13 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,53 @@
+require 'pry'
+
+animals = {
+  "sugar glider"=>"Australia",
+  "aye-aye"=> "Madagascar",
+  "red-footed tortoise"=>"Panama",
+  "kangaroo"=> "Australia",
+  "tomato frog"=>"Madagascar",
+  "koala"=>"Australia"
+}
+
+# Return array with every key marching input arguments
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    key_array = []
+    
+    # How to access hash?
+    print self 
+    
+
+    # Cycle through arguments to match with hash
+=begin
+    arguments.each do |argument|
+      map do |key, value|
+        key_array << key if argument == value 
+      end
+    end 
+=end    
+    key_array
+    
+    # binding.pry 
+    
   end
 end
+
+animals.keys_of('Panama')   
+
+    
+
+=begin
+  it "returns red-footed tortoise in an array for the animal hash when passed the argument 'Panama'" do
+    result = animals.keys_of('Panama')
+    expect(result).to include("red-footed tortoise")
+    expect(result.length).to eq(1)
+  end
+
+  it "returns two species in an array for the animal hash when passed the argument 'Madagascar'" do
+    result = animals.keys_of('Madagascar')
+    ["aye-aye", "tomato frog"].each {|animal| expect(result).to include(animal) }
+    expect(result.length).to eq(2)
+  end
+=end 
+
