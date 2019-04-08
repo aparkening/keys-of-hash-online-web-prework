abdf9c0ecaca111915b6c02e15cb8a709b2be29a
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-08 21:26:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -25,24 +25,3 @@ class Hash
     key_array
   end
 end
-
-
-#animals.keys_of('Australia')  
-{a: 1, b: 2, c: 3, d: 1}.keys_of(1, 2)
-
-    
-
-=begin
-  it "returns red-footed tortoise in an array for the animal hash when passed the argument 'Panama'" do
-    result = animals.keys_of('Panama')
-    expect(result).to include("red-footed tortoise")
-    expect(result.length).to eq(1)
-  end
-
-  it "returns two species in an array for the animal hash when passed the argument 'Madagascar'" do
-    result = animals.keys_of('Madagascar')
-    ["aye-aye", "tomato frog"].each {|animal| expect(result).to include(animal) }
-    expect(result.length).to eq(2)
-  end
-=end 
-
